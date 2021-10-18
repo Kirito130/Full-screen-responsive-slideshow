40de35bf4db053c4609e20aac25c03f672c9c040
Kirito <taiyablokhandwala234@gmail.com>
2021-10-18 18:06:27 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true;
+const interval = 5000;
+let slideInterval;
+
+const nextSlide = () => { 
+    // Get Current Class
+    const current = document.querySelector('.current');
+
+    // Remove Current Class
+    current.classList.remove('current');
+
+    // Check for next slide
+    if (current.nextElementSibling) {
+        // Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        // Add current to start
+        slides[0].classList.add('current');
+    }
+
+    setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+    // Get Current Class
+    const current = document.querySelector('.current');
+
+    // Remove Current Class
+    current.classList.remove('current');
+
+    // Check for next slide
+    if (current.previousElementSibling) {
+        // Add current to next sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        // Add current to last
+        slides[slides.length - 1].classList.add('current');
+    }
+
+    setTimeout(() => current.classList.remove('current'));
+}
+
+// Button Events
+
+next.addEventListener('click', e => {
+    nextSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, interval);
+    }
+});
+
+prev.addEventListener('click', e => {
+    prevSlide();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide, interval);
+    }
+});
+
+// Auto Slide
+if (auto) {
+    // Run next slide at interval time
+    slideInterval = setInterval(nextSlide, interval);
+}
+
